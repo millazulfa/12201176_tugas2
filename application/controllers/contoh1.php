019767d97d6ae620d8b420ea90c38a2ef2563bfb
millazulfa <miljinzulfa0315@gmail.com>
2021-10-14 10:55:32 +0700

    menambahkan tugas 2 dan biodata

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,9 @@
+<?php
+class contoh1 extends CI_Controller
+{
+    echo "<h1>perkenalkan</h1>";
+    echo"Nama saya Imam Nawawi
+        saya tinggal di daerah ciputat
+        olah raga yang saya sukai adalah
+        bulutangkis";
+}
